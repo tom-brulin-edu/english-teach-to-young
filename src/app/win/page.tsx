b7cde6cf7ab0787c724d9772f844954468ff079d
tom-brulin <t.brulin@outlook.com>
2023-06-08 10:10:30 +0200

    feat(*): fix the windows usage for width and height

--- src/app/win/page.tsx
+++ src/app/win/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Confetti from 'react-confetti'
+import Confetti from "react-confetti";
+import { useScreen } from "usehooks-ts";
 
 export default function Page() {
+  const screen = useScreen();
 
-    const {innerWidth: width, innerHeight: height} = window;
-
-    return (
-        <>
-            <h1 className={"text-green-500 text-9xl text-center m-auto"} >You Won</h1>
-            <Confetti width={innerWidth} height={innerHeight}/>
-        </>
-    )
-};
+  return (
+    <>
+      <h1 className={"text-green-500 text-9xl text-center m-auto"}>You Won</h1>
+      <Confetti width={screen?.width} height={screen?.height} />
+    </>
+  );
+}
